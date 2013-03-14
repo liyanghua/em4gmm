5b7824e4b0513e1c3b725421b7eb2dfc5fcfbbc3
Juan Daniel <juanda.vm@gmail.com>
2013-03-14 09:04:40 +0100

    English comments on the training utility.

--- train.c
+++ train.c
@@ -17,19 +17,19 @@ int main(int argc,char *argv[]) {
 	if(argc==4||argc==5){
 		decimal last=INT_MIN,llh,sigma=0.1;
 		number i;
-		if(argc==5)sigma=atof(argv[4]);
-		data *feas=feas_load(argv[2]);
-		gmm *gmix=gmm_initialize(feas,atoi(argv[1]));
-		for(i=1;i<101;i++){
-			llh=gmm_EMtrain(feas,gmix);
+		if(argc==5)sigma=atof(argv[4]); /* Retrieve sigma threshold to stop the EM.    */
+		data *feas=feas_load(argv[2]); /* Load the features from the specified file.   */
+		gmm *gmix=gmm_initialize(feas,atoi(argv[1])); /* Do a good GMM initialization. */
+		for(i=1;i<101;i++){ /* We set 100 maximum iterations to obtain EM convergence. */
+			llh=gmm_EMtrain(feas,gmix); /* Do one iteration of the EM algorithm.   */
 			printf("Iteration: %03i    Improvement: %3i%c    LogLikelihood: %.3f\n",
-				i,abs(round(-100*(llh-last)/last)),'%',llh);
-			if(last-llh>-sigma||isnan(last-llh)) break;
+				i,abs(round(-100*(llh-last)/last)),'%',llh); /* Show EM data.  */
+			if(last-llh>-sigma||isnan(last-llh)) break; /* Test sigma threshold.   */
 			last=llh;
 		}
 		feas_delete(feas);
-		gmm_init_classifier(gmix); /* Inicializamos para ganar velocidad al reconocer. */
-		gmm_save(argv[3],gmix);
+		gmm_init_classifier(gmix); /* Pre-compute the non-data dependant part of classifier. */
+		gmm_save(argv[3],gmix); /* Save the model with the pre-computed part for classify.   */
 		gmm_delete(gmix);
 	}else fprintf(stderr,"Usage: %s <mixtures> <features> <model> [sigma]\n",argv[0]);
 	return 0;
