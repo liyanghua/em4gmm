32aa8dfdd2bda635f6bb47e5a097ee3bf4376865
Juan Daniel <juanda.vm@gmail.com>
2013-03-20 14:58:27 +0100

    Old zlib bug corrected on Ubuntu.

--- src/data.c
+++ src/data.c
@@ -54,7 +54,6 @@ data *feas_load(char *filename){
 	t->s=t->d=t->c=t->point=t->next=0,t->header=2,t->dec=t->sign=1;
 	number i,r; t->feas=feas,t->buff=NULL;
 	gzFile f=gzopen(filename,"r"); /* Read the file using zlib library. */
-	gzbuffer(f,128*1024);
 	if(!f) fprintf(stderr,"Error: Not %s feature file found.\n",filename),exit(1);
 	while(!gzeof(f)){
 		char *buff=(char*)calloc(SIZE_BUFFER,sizeof(char));
